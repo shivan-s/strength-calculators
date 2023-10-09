7b73ce56d004246bbdf4d4ab380c3b374343035c
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-10-09 23:27:23 +1300

    style: fixing style Completes #19

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import RpeSelect from './RpeSelect';
 import Sidebar from './Sidebar';
-import Loading from './Loading'
+import Loading from './Loading';
 
 export { Navbar, Footer, RpeSelect, Sidebar, Loading };

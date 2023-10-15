4167245d5a214d1a65e245dcf02845312fd9dfd6
Shivan Sivakumaran <shivan@Shivans-MacBook-Pro.local>
2023-10-15 22:58:38 +1300

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Welcome</h1>
+<script lang="ts">
+	import Button from '$components/button/Button.svelte';
+	import Icon from '@iconify/svelte';
+</script>
 
-<p>Strength Calculators is a work in progress.</p>
+<h1 class="text-6xl">
+	<span>Strength Calculators</span>
+</h1>
+
+<form method="get" action="/?q">
+	<div class="flex">
+		<input class="input input-primary" />
+		<Button>
+			<Icon class="2xl" icon="ph:calculator" />
+		</Button>
+	</div>
+</form>
+

57c0fe5993789e6123d127d8f4f3a6c013cd4f84
Shivan Sivakumaran <me@shivan.xyz>
2023-10-18 09:16:04 +1300

    wip: adding id tag WIP: #24

--- src/components/result/Result.svelte
+++ src/components/result/Result.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex w-full justify-center">
 	<div class="stat w-fit rounded-md shadow">
-		<div class="stat-title">{title}</div>
+		<div id="result" class="stat-title">{title}</div>
 		<div class="stat-value">{value}</div>
 	</div>
 </div>
